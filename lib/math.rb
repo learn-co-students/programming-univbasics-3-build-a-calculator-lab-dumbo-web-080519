d5e071440ba561a051a215074718646c2cffae38
Brian Wong <bwong92@gmail.com>
2019-07-30 01:04:31 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,25 @@
 def addition(num1, num2)
-
+   num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  num1 - num2
 end
 
 def division(num1, num2)
-
+   num1 / num2
 end
 
 def multiplication(num1, num2)
-
+   num1 * num2
 end
 
 def modulo(num1, num2)
-
+  num1 % num2
 end
 
 def square_root(num)
-
+  (1..num).each do |i|
+    return i if i * i == num
+  end
 end
